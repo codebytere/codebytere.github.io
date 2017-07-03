2318305a78363780a6536c54526b6ab4f168d362
Shelley <csvohr1995@gmail.com>
2017-07-03 00:51:01 -0700

    add modals and clicking functionality

--- main.js
+++ main.js
@@ -1,18 +1,43 @@
+const items = {
+  '#notebook': 'Projects',
+  '#headphones': 'About Me',
+  '#macbook': 'Skills',
+  '#mail': 'Contact',
+  '#iphone': 'Links',
+  '#flash-drive': 'Resume',
+  '#yubikey': 'Keybase',
+};
+
 $(() => {
-  $('#stage').load('landing.svg', (response) => {
-    $(this).addClass('svgLoaded');
-    if (!response) {
-      console.log('Error');
-    }
-
-    $('#notebook').tooltip({ title: 'GET /projects', container: 'body' });
-    $('#about-me').tooltip({ title: 'GET /about', container: 'body' });
-    $('#headphones').tooltip({ title: 'GET /about', container: 'body' });
-    $('#macbook').tooltip({ title: 'GET /skills', container: 'body' });
-    $('#mail').tooltip({ title: 'POST /contact', container: 'body' });
-    $('#iphone').tooltip({ title: 'GET /links', container: 'body' });
-    $('#yubikey').tooltip({ title: 'GET /keybase', container: 'body' });
-
-    $('#notebook').click((e) => { $('#myModal').modal('show'); });
+  $('#stage').load('assets/landing.svg', (response) => {
+    loadTooltips();
+    setEvents();
   });
+  $.get('html/modal.html', (modal) => { $('body').append(modal); });
 });
+
+function loadTooltips() {
+  Object.keys(items).forEach((key) => {
+    $(`${key}`)
+      .attr('cursor', 'pointer')
+      .addClass('button')
+      .tooltip({
+        title: `${items[key]}`,
+        container: 'body',
+        content: () => `${items[key]}`,
+      });
+  });
+}
+
+function setEvents() {
+  Object.keys(items).forEach((key) => {
+    $(`${key}`).on('mouseup', (ev) => {
+      $('#myModal').modal('show');
+
+      const source = `html/${items[key].toLowerCase()}.html`;
+      $('.modal-header').html(`<h2>${items[key]}</h2>`);
+      $.get(source, (contents) => { $('.modal-body').html(contents); });
+      ev.preventDefault();
+    });
+  });
+}
