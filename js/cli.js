334e119c3fa32aac1b8e3cb0c32a381d008931b5
Shelley Vohr <shelley.vohr@gmail.com>
2018-02-24 09:40:46 -0500

    switch to standardjs

--- js/cli.js
+++ js/cli.js
@@ -1,116 +1,97 @@
+/* global $, localStorage */
+
 const errors = {
   invalidDirectory: 'Error: not a valid directory',
   noWriteAccess: 'Error: you do not have write access to this directory',
-  fileNotFound: 'Error: file not found in current directory',
-};
+  fileNotFound: 'Error: file not found in current directory'
+}
 
 const struct = {
   root: ['about', 'resume', 'contact'],
   projects: ['nodemessage', 'map', 'dotify', 'slack_automation'],
-  skills: ['proficient', 'familiar', 'learning'],
-};
-
-function placeCaretAfter(el) {
-  el.focus();
-  if (typeof window.getSelection !== 'undefined' &&
-      typeof document.createRange !== 'undefined') {
-    const range = document.createRange();
-    range.setStartAfter(el);
-    range.collapse(true);
-
-    const sel = window.getSelection();
-    sel.removeAllRanges();
-    sel.addRange(range);
-  } else if (typeof document.body.createTextRange !== 'undefined') {
-    const textRange = document.body.createTextRange();
-    textRange.moveToElementText(el);
-    textRange.collapse(false);
-    textRange.select();
-  }
+  skills: ['proficient', 'familiar', 'learning']
 }
 
-function getDirectory() {
-  return localStorage.directory;
+const commands = {}
+let systemData = {}
+const rootPath = 'users/codebytere/root'
+
+const getDirectory = () => localStorage.directory
+
+const setDirectory = (dir) => {
+  localStorage.directory = dir
 }
 
-function setDirectory(dir) {
-  localStorage.directory = dir;
-}
-
-function registerFullscreenToggle() {
+function registerFullscreenToggle () {
   $('.button.green').click(() => {
-    $('.terminal-window').toggleClass('fullscreen');
-  });
+    $('.terminal-window').toggleClass('fullscreen')
+  })
 }
 
-const commands = {};
-let systemData = {};
-const rootPath = 'users/codebytere/root';
+// create new directory in current directory
+commands.mkdir = () => errors.noWriteAccess
 
 // create new directory in current directory
-commands.mkdir = () => errors.noWriteAccess;
-
-// create new directory in current directory
-commands.touch = () => errors.noWriteAccess;
+commands.touch = () => errors.noWriteAccess
 
 // remove file from current directory
-commands.rm = () => errors.noWriteAccess;
+commands.rm = () => errors.noWriteAccess
 
 // view contents of current directory
-commands.ls = () => systemData[getDirectory()];
+commands.ls = () => systemData[getDirectory()]
 
 // view list of possible commands
-commands.help = () => systemData.help;
+commands.help = () => systemData.help
 
 // display current path
 commands.path = () => {
-  const dir = getDirectory();
-  return dir === 'root' ? rootPath : `${rootPath}/${dir}`;
-};
+  const dir = getDirectory()
+  return dir === 'root' ? rootPath : `${rootPath}/${dir}`
+}
 
 // see command history
 commands.history = () => {
-  let history = localStorage.history;
-  history = history ? Object.values(JSON.parse(history)) : [];
-  return `<p>${history.join('<br>')}</p>`;
-};
+  let history = localStorage.history
+  history = history ? Object.values(JSON.parse(history)) : []
+  return `<p>${history.join('<br>')}</p>`
+}
 
 // move into specified directory
 commands.cd = (newDirectory) => {
-  const currDir = getDirectory();
-  const dirs = ['root', 'projects', 'skills'];
-  const newDir = newDirectory ? newDirectory.trim() : '';
+  const currDir = getDirectory()
+  const dirs = ['root', 'projects', 'skills']
+  const newDir = newDirectory ? newDirectory.trim() : ''
 
   if (dirs.includes(newDir) && currDir !== newDir) {
-    setDirectory(newDir);
+    setDirectory(newDir)
   } else if (newDir === '') {
-    setDirectory('root');
+    setDirectory('root')
   } else {
-    return errors.invalidDirectory;
+    return errors.invalidDirectory
   }
-  return null;
-};
+  return null
+}
 
 // display contents of specified file
 commands.cat = (filename) => {
-  const dir = getDirectory();
-  const fileKey = filename.split('.')[0];
+  const dir = getDirectory()
+  const fileKey = filename.split('.')[0]
 
   if (fileKey in systemData && struct[dir].includes(fileKey)) {
-    return systemData[fileKey];
+    return systemData[fileKey]
   }
 
-  return errors.fileNotFound;
-};
+  return errors.fileNotFound
+}
 
 // initialize cli
 $(() => {
-  registerFullscreenToggle();
-  const cmd = document.getElementById('terminal');
-  const terminal = new shell(cmd, commands);
+  registerFullscreenToggle()
+  const cmd = document.getElementById('terminal')
+  const terminal = new Shell(cmd, commands)
 
-  $.ajaxSetup({ cache: false });
+  $.ajaxSetup({ cache: false })
   $.get('data/system_data.json', (data) => {
-    systemData = data;
-  });
-});
+    systemData = data
+  })
+})
---
+++ js/shell.js
@@ -0,0 +1,104 @@
+/* global $, localStorage */
+
+class Shell {
+  constructor (term, commands) {
+    this.commands = commands
+    this.setupListeners(term)
+    this.term = term
+
+    localStorage.directory = 'root'
+    localStorage.history = JSON.stringify('')
+
+    $('.input').focus()
+  }
+
+  setupListeners (term) {
+    $('#terminal').mouseup(() => {
+      $('.input').last().focus()
+    })
+
+    term.addEventListener('keyup', (evt) => {
+      const keyUp = 38
+      const keyDown = 40
+      const key = evt.keyCode
+
+      if ([keyUp, keyDown].includes(key)) {
+        let history = localStorage.history
+        history = history ? Object.values(JSON.parse(history)) : []
+
+        if (key === keyUp && localStorage.historyIndex >= 0) {
+          $('.input').last().html(`${history[localStorage.historyIndex]}<span class="end"><span>`)
+          localStorage.historyIndex -= 1
+        } else if (key === keyDown && localStorage.historyIndex < history.length) {
+          $('.input').last().html(history[localStorage.historyIndex])
+          localStorage.historyIndex += 1
+        }
+        evt.preventDefault()
+        $('.end').focus()
+      }
+    })
+
+    term.addEventListener('keypress', (evt) => {
+      if (evt.keyCode === 13) {
+        localStorage.historyIndex = 0
+        const prompt = evt.target
+        const input = prompt.textContent.trim().split(' ')
+        const cmd = input[0]
+        const args = input[1]
+
+        if (cmd === 'clear') {
+          this.clearConsole()
+        } else if (cmd && cmd in this.commands) {
+          this.runCommand(cmd, args)
+          this.resetPrompt(term, prompt)
+          $('.root').last().html(localStorage.directory)
+        } else {
+          this.term.innerHTML += 'Error: command not recognized'
+          this.resetPrompt(term, prompt)
+        }
+        evt.preventDefault()
+      }
+    })
+  }
+
+  runCommand (cmd, args) {
+    const command = args ? `${cmd} ${args}` : cmd
+    this.updateHistory(command)
+
+    const output = this.commands[cmd](args)
+    if (output) { this.term.innerHTML += output }
+  }
+
+  resetPrompt (term, prompt) {
+    const newPrompt = prompt.parentNode.cloneNode(true)
+    prompt.setAttribute('contenteditable', false)
+    if (this.prompt) {
+      newPrompt.querySelector('.prompt').textContent = this.prompt
+    }
+    term.appendChild(newPrompt)
+    newPrompt.querySelector('.input').innerHTML = ''
+    newPrompt.querySelector('.input').focus()
+  }
+
+  updateHistory (command) {
+    let history = localStorage.history
+    history = history ? Object.values(JSON.parse(history)) : []
+
+    history.push(command)
+    localStorage.history = JSON.stringify(history)
+    localStorage.historyIndex = history.length - 1
+  }
+
+  clearConsole () {
+    $('#terminal').html(
+      `<p class="hidden">
+        <span class="prompt">
+          <span class="root">root</span>
+          <span class="tick">❯</span>
+        </span>
+        <span contenteditable="true" class="input"></span>
+      </p>`
+    )
+    $('.input').focus()
+  }
+}
