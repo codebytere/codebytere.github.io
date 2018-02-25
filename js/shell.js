5372136b87865b3751b3e564e9b7ea314860be6a
Shelley Vohr <shelley.vohr@gmail.com>
2018-02-25 07:36:07 -0800

    remove stray console.log

--- js/shell.js
+++ js/shell.js
@@ -44,7 +44,6 @@ class Shell {
         evt.preventDefault()
       // escape key is pressed
       } else if (evt.keyCode === 27) {
-        console.log('hello')
         $('.terminal-window').toggleClass('fullscreen')
       }
     })
